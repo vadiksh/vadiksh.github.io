6ec2a1ef653b0b19b362c8b5bb543f27492c1223
Vadim <vadik.shevchenko@gmail.com>
2017-11-29 15:43:25 +0300

    fix validation bug

--- homepage/js/index.js
+++ homepage/js/index.js
@@ -7,19 +7,21 @@ $('.access__form').submit(function (event) {
   
 
   if (name.length < 3) {
-  	displayError('enter your name');
+    event.preventDefault();
+    displayError('enter your name');
   } else if (email.length === 0) {
-  	displayError('enter your email');
+    event.preventDefault();
+    displayError('enter your email');
   } else if (!emailRegEx.test(email)) {
-  	displayError('make sure you enter a valid email');
+    event.preventDefault();
+    displayError('make sure you enter a valid email');
   } else {
-  	alert('VALIDATED!');
+    alert('VALIDATED!');
   }
 });
 
 function displayError(errortext) {
-  event.preventDefault();
-  	error.slideUp(250, function() {
-  	  error.html('Please' + ' ' + errortext + '.').slideDown(500);
-  	});
+    error.slideUp(250, function() {
+      error.html('Please' + ' ' + errortext + '.').slideDown(500);
+    });
 }
