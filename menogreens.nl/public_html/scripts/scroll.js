f91ab3045165a64178d9596d737d5d5a17a9d51f
Vadim <vadik.shevchenko@gmail.com>
2018-02-24 13:42:43 +0300

    changes

--- menogreens.nl/public_html/scripts/scroll.js
+++ menogreens.nl/public_html/scripts/scroll.js
@@ -1,20 +1,14 @@
 $(document).ready(function(){
 
 	$('.faq-question').click(function(event) {
-		if ($(event.target).siblings().hasClass('active')) {
-			$('.active').next().slideUp();
-			$(event.target).siblings().removeClass('active');
-		}
-		$(event.target).addClass('active').next().slideToggle();
-	})
-
-
-	$('a[href*=#]').bind("click", function(e){
-	var anchor = $(this);
-	$('html, body').stop().animate({
-	scrollTop: $(anchor.attr('href')).offset().top
-	}, 500);
-	e.preventDefault();
+		$(event.target).next().slideToggle();
 	});
-	return false;
+
+	$('a[href*="#"]').click(function(e) {
+		e.preventDefault();
+		var position = $($(this).attr('href')).offset().top;
+		$("html, body").animate({
+			scrollTop: position
+		});
+	});
 });
