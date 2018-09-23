cb5d5e33bbe324e5ca2eadaa4ea2af65ada10a74
Vadim <vadik.shevchenko@gmail.com>
2018-09-23 22:44:14 +0300

    +completed responsiveness
    only products popup needed

--- thyssen/js/index.js
+++ thyssen/js/index.js
@@ -1,3 +1,23 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	loop:true,
+  	margin:0,
+  	nav:true,
+  	center:true,
+  	responsive:{
+  	    0:{
+  	        items:1.4
+  	    },
+  	    400:{
+  	        items:1.8
+  	    },
+  	    640:{
+  	        items:3
+  	    }
+  	}
+  });
+});
+
 $('.mob-menu-open').click(function() {
 	if ($('.mob-menu').hasClass('revealed')) {
 		$('.mob-menu').removeClass('revealed');
@@ -12,3 +32,4 @@ $('.mob-menu-open').click(function() {
 		$('.mob-logo').addClass('revealed');
 	}
 });
+
