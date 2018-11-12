0fb45461085223cc2656689db6fa02131cc72f84
Vadim <vadik.shevchenko@gmail.com>
2018-11-12 17:42:56 +0200

    added parallax

--- lupo/js/common.js
+++ lupo/js/common.js
@@ -1,5 +1,24 @@
-$(function() {
+$(document).ready(function() {
 
-	// Custom JS
+  $('.container').parallax({imageSrc: 'img/fondo.jpg'});
 
+  var item = $('.screenshot-item');
+  var img = $('.screenshot-item .screenshot-img');
+  var modal = $('#myModal');
+  var modalImg = $("#img01");
+  var captionText = $(".caption");
+
+  for (var i = 0; i < item.length; i++) {
+  	item[i].onclick = function(){
+  	  var src = $(this).children().attr("src");
+      modal.css({"display" : "block"});
+      modalImg.attr('src', src);
+  	}
+  }
+
+  var closeModal = $(".close-modal");
+
+  closeModal.click(function() {
+    modal.css({"display" : "none"});
+ })
 });
