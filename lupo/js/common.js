23ddf9bf2e046244eec288785a5388d2a70a14c3
Vadim <vadik.shevchenko@gmail.com>
2018-11-13 14:10:14 +0200

    fixes

--- lupo/js/common.js
+++ lupo/js/common.js
@@ -19,25 +19,68 @@ $(document).ready(function() {
     $('.container').parallax({imageSrc: 'img/fondo.jpg'});
   }
   
-  
-
   var item = $('.screenshot-item');
   var img = $('.screenshot-item .screenshot-img');
   var modal = $('#myModal');
-  var modalImg = $("#img01");
-  var captionText = $(".caption");
+  var modalImg = $(".modal-content");
+  var prev = $(".modal-prev");
+  var next = $(".modal-next");
+  var close = $(".modal-close");
 
   for (var i = 0; i < item.length; i++) {
   	item[i].onclick = function(){
   	  var src = $(this).children().attr("src");
-      modal.css({"display" : "block"});
+      modal.css({"display" : "flex"});
       modalImg.attr('src', src);
   	}
   }
 
-  var closeModal = $(".close-modal");
+  prev.click(function() {
+    var modalSrc = modalImg.attr('src');
+    for (var j = 0; j < img.length; j++) { 
+      if (img[j].id == modalSrc) {
+        modalImg.attr('src', img[j - 1].id)
+      }
+    }
+  });
 
-  closeModal.click(function() {
+  next.click(function() {
+    var modalSrc = modalImg.attr('src');
+    for (var k = 0; k < img.length; k++) {
+      if (img[k].id == modalSrc) {
+        modalImg.attr('src', img[k + 1].id)
+      }
+    }
+  });
+
+  close.click(function() {
     modal.css({"display" : "none"});
- })
+  });
 });
+
+// <ul class="screenshot">
+//   <li class="screenshot-item">
+//     <img src="img/ProfessorLupo11.jpg" class="screenshot-img">
+//   </li>
+//   <li class="screenshot-item">
+//     <img src="img/ProfessorLupo05.jpg" class="screenshot-img">
+//   </li>
+//   <li class="screenshot-item">
+//     <img src="img/ProfessorLupo01.jpg" class="screenshot-img">
+//   </li>
+//   <li class="screenshot-item">
+//     <img src="img/ProfessorLupo04.jpg" class="screenshot-img">
+//   </li>
+//   <li class="screenshot-item">
+//     <img src="img/ProfessorLupo03.jpg" class="screenshot-img">
+//   </li>
+//   <li class="screenshot-item">
+//     <img src="img/ProfessorLupo02.jpg" class="screenshot-img">
+//   </li>
+// </ul>
+// <div id="myModal" class="modal">
+//   <span class="close-modal">&times;</span>
+//   <img class="modal-content" id="img01" src="">
+//   <span class="modal-prev">&lt;</span>
+//   <span class="modal-next">&gt;</span>
+// </div>
