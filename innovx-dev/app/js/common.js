28a76773a47dc7ccc4bebe45d8e3e60a6d3b7e1f
Vadim <vadik.shevchenko@gmail.com>
2019-04-04 02:55:45 +0300

    apply adway + innovx

---
+++ innovx-dev/app/js/common.js
@@ -0,0 +1,51 @@
+$(function() {
+
+	if ($(window).width() < 950) {
+		$('.holdings__list').owlCarousel({
+		    loop:false,
+		    nav: false,
+		    items: 2,
+		    responsiveClass:true,
+		    responsive:{
+		        0:{
+		            items:2,
+		            margin: 20
+		        },
+		        630: {
+		        	items: 3
+		        },
+		        767: {
+		        	items: 4,
+		        	margin: 50
+		        }
+		    }
+		})
+	}
+
+	$(window).resize(function() {
+		if ($(window).width() < 950) {
+			$('.holdings__list').owlCarousel({
+			    loop:false,
+			    nav: false,
+			    items: 2,
+			    responsiveClass:true,
+			    responsive:{
+			        0:{
+			            items:2,
+			            margin: 20
+			        },
+			        630: {
+			        	items: 3
+			        },
+			         767: {
+    		        	items: 4,
+    		        	margin: 50
+    		        }
+			    }
+			})
+		} else {
+			$('.holdings__list').trigger('destroy.owl.carousel');
+		}
+	})
+	
+});
