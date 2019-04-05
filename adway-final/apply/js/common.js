e35f90a6183a8b4446e997f12431010198e80c38
Vadim <vadik.shevchenko@gmail.com>
2019-04-05 13:04:09 +0300

    apply animations

--- adway-final/apply/js/common.js
+++ adway-final/apply/js/common.js
@@ -1,21 +1,18 @@
 // $(function() {
 	$(window).on('load', function() {
-		if ($('.job-screen').length) {
-			$('.job-screen .container').css({
-				"opacity": 1
-			})
-			$('.job-screen__description .shaved p').shave(100);
-			if ($(window).width() < 350) {
-				$('.job-screen__description .shaved p').shave(60);
-			}
+		$('.job-screen .container').css({
+			"opacity": 1
+		})
+		$('.job-screen__description .shaved p').shave(100);
+		if ($(window).width() < 350) {
+			$('.job-screen__description .shaved p').shave(60);
 		}
 
-		if ($('.condensed')) {
-			var condensedHeight = $('.shaved').height();
-			$('.condensed').css({
-				'height': condensedHeight + 'px'
-			})
-		}
+		var condensedHeight = $('.shaved').height();
+		$('.condensed').css({
+			'height': condensedHeight + 'px'
+		})
+
 		$('.show-more').click(function() {
 			var condensedHeight = $('.shaved').height(),
 				expandedHeight = document.getElementsByClassName('condensed')[0].scrollHeight;
@@ -25,15 +22,20 @@
 
 			if ($('.job-screen').hasClass('revealed')) {
 				$('.job-screen').removeClass('revealed');
-				// $('.overflow-wrapper').animate({
-				// 	scrollTop: 0
-				// }, 200)
-				document.getElementsByClassName('condensed')[0].style.height = condensedHeight + "px"
+				$('.overflow-wrapper').animate({
+					scrollTop: 0
+				}, 200)
+				document.getElementsByClassName('condensed')[0].style.height = condensedHeight + "px";
+				// document.getElementsByClassName('apply-button').style.animation = "bounce .8s forwards";
+				for (var i = 0; i < document.getElementsByClassName('apply-button').length; i++) {
+					document.getElementsByClassName('apply-button')[i].style.animation = "bounce .8s forwards";
+				}
 			} else {
 				$('.job-screen').addClass('revealed');
 				document.getElementsByClassName('condensed')[0].style.height = expandedHeight + "px"
 			}
 		})
+
 		$('.screen1, .form-screen').addClass('active');
 		$('.form-screen__button').click(function(e) {
 			e.preventDefault();
@@ -46,6 +48,11 @@
 				scrollTop: 0
 			}, 200)
 		})
+		$('.change-photo button').click(function(e) {
+			e.preventDefault();
+			$('.change-photo-modal').addClass('visible');
+		})
+
 	})
 
 	
