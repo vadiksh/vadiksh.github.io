9de7c0f6607680ca973751d9d4832f779c39f863
Vadim <vadik.shevchenko@gmail.com>
2019-04-05 11:54:34 +0300

    adway animations

--- adway-final/apply/js/scripts.min.js
+++ adway-final/apply/js/scripts.min.js
@@ -1,43 +1,53 @@
-$(function() {
+// $(function() {
+	$(window).on('load', function() {
+		if ($('.job-screen').length) {
+			$('.job-screen .container').css({
+				"opacity": 1
+			})
+			$('.job-screen__description .shaved p').shave(100);
+			if ($(window).width() < 350) {
+				$('.job-screen__description .shaved p').shave(60);
+			}
+		}
+
+		if ($('.condensed')) {
+			var condensedHeight = $('.shaved').height();
+			$('.condensed').css({
+				'height': condensedHeight + 'px'
+			})
+		}
+		$('.show-more').click(function() {
+			var condensedHeight = $('.shaved').height(),
+				expandedHeight = document.getElementsByClassName('condensed')[0].scrollHeight;
+			$('.condensed').css({
+				'height': condensedHeight + 'px'
+			})
+
+			if ($('.job-screen').hasClass('revealed')) {
+				$('.job-screen').removeClass('revealed');
+				// $('.overflow-wrapper').animate({
+				// 	scrollTop: 0
+				// }, 200)
+				document.getElementsByClassName('condensed')[0].style.height = condensedHeight + "px"
+			} else {
+				$('.job-screen').addClass('revealed');
+				document.getElementsByClassName('condensed')[0].style.height = expandedHeight + "px"
+			}
+		})
+		$('.screen1, .form-screen').addClass('active');
+		$('.form-screen__button').click(function(e) {
+			e.preventDefault();
+
+			var index = $('.screen.active').attr('data-index');
+			$('.form-screen__progress').addClass('step-' + index);
+			$('.screen' + index).addClass('active').siblings().removeClass('active');
+
+			$('.overflow-wrapper').animate({
+				scrollTop: 0
+			}, 200)
+		})
+	})
 
 	
-	if ($('.job-screen').length) {
-		$('.job-screen__description .shaved p').shave(100);
-		if ($(window).width() < 350) {
-			$('.job-screen__description .shaved p').shave(60);
-		}
-	}
-
-	var condensedHeight = $('.shaved').height(),
-		expandedHeight = document.getElementsByClassName('condensed')[0].scrollHeight;
-	$('.condensed').css({
-		'height': condensedHeight + 'px'
-	})
-	$('.show-more').click(function() {
-		
-		if ($('.job-screen').hasClass('revealed')) {
-			$('.job-screen').removeClass('revealed');
-			$('.overflow-wrapper').animate({
-				scrollTop: 0
-			}, 200)
-			document.getElementsByClassName('condensed')[0].style.height = condensedHeight + "px"
-		} else {
-			$('.job-screen').addClass('revealed');
-			document.getElementsByClassName('condensed')[0].style.height = expandedHeight + "px"
-		}
-	})
-	$('.form-screen__button').click(function(e) {
-		e.preventDefault();
-		if (true) { // $('.screen.active').validate()
-			var index = $('.screen.active').attr('data-index');
-			
-			$('.form-screen__progress').addClass('step-' + index);
-			$('.screen:nth-of-type(' + index + ')').addClass('active').siblings().removeClass('active');
-			$('.overflow-wrapper').animate({
-				scrollTop: 0
-			}, 200)
-		}
-					
-			
-	})
-});
+	
+// });
