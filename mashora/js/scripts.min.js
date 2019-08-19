2f1d56349bae67c8a21e490623698408c38822a8
Vadim <vadik.shevchenko@gmail.com>
2019-08-19 18:34:32 +0300

    uploaded mashora

---
+++ mashora/js/scripts.min.js
@@ -0,0 +1,29 @@
+$(function() {
+	var scrolled,
+		scrolledBottom;
+	$(window).scroll(function() {
+		scrolled = $(this).scrollTop() + $(window).height()/2 - 115;
+		scrolledBottom = scrolled + 230;
+
+		
+		if (scrolled > $('.how-we-work__list').offset().top &&
+			scrolledBottom < $('.how-we-work .button').offset().top) {
+			$('.how-we-work__sidebar').addClass('visible');
+		} else {
+			$('.how-we-work__sidebar').removeClass('visible');
+		}
+
+		if (scrolled > $('.how-we-work__list').offset().top &&
+			scrolled < $('.how-we-work__list li:nth-of-type(2)').offset().top) {
+			$('.how-we-work__sidebar li').eq(0).addClass('active').siblings().removeClass('active');
+		} else if (scrolled > $('.how-we-work__list li:nth-of-type(2)').offset().top &&
+				   scrolled < $('.how-we-work__list li:nth-of-type(3)').offset().top) {
+			$('.how-we-work__sidebar li').eq(1).addClass('active').siblings().removeClass('active');
+		} else if (scrolled > $('.how-we-work__list li:nth-of-type(3)').offset().top && 
+				   scrolledBottom < $('.how-we-work__list li:nth-of-type(4)').offset().top) {
+			$('.how-we-work__sidebar li').eq(2).addClass('active').siblings().removeClass('active');
+		} else if (scrolled > $('.how-we-work__list li:nth-of-type(4)').offset().top) {
+			$('.how-we-work__sidebar li').eq(3).addClass('active').siblings().removeClass('active');
+		}
+	});
+});
