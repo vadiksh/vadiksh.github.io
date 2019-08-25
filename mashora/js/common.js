b90752415054487191fe099891b1f4e229dc03de
Vadim <vadik.shevchenko@gmail.com>
2019-08-25 14:13:20 +0300

    updated mashora

--- mashora/js/common.js
+++ mashora/js/common.js
@@ -1,44 +1,37 @@
 $(function() {
-	// var scrolled,
-	// 	scrolledBottom;
-	// $(window).scroll(function() {
-	// 	scrolled = $(this).scrollTop() + $(window).height()/2 - 115;
-	// 	scrolledBottom = scrolled + 230;
 
-		
-	// 	if (scrolled > $('.how-we-work__list').offset().top &&
-	// 		scrolledBottom < $('.how-we-work .button').offset().top) {
-	// 		$('.how-we-work__sidebar').addClass('visible');
-	// 	} else {
-	// 		$('.how-we-work__sidebar').removeClass('visible');
-	// 	}
+	$('.header').addClass('visible');
 
-	// 	if (scrolled > $('.how-we-work__list').offset().top &&
-	// 		scrolled < $('.how-we-work__list li:nth-of-type(2)').offset().top) {
-	// 		$('.how-we-work__sidebar li').eq(0).addClass('active').siblings().removeClass('active');
-	// 	} else if (scrolled > $('.how-we-work__list li:nth-of-type(2)').offset().top &&
-	// 			   scrolled < $('.how-we-work__list li:nth-of-type(3)').offset().top) {
-	// 		$('.how-we-work__sidebar li').eq(1).addClass('active').siblings().removeClass('active');
-	// 	} else if (scrolled > $('.how-we-work__list li:nth-of-type(3)').offset().top && 
-	// 			   scrolledBottom < $('.how-we-work__list li:nth-of-type(4)').offset().top) {
-	// 		$('.how-we-work__sidebar li').eq(2).addClass('active').siblings().removeClass('active');
-	// 	} else if (scrolled > $('.how-we-work__list li:nth-of-type(4)').offset().top) {
-	// 		$('.how-we-work__sidebar li').eq(3).addClass('active').siblings().removeClass('active');
-	// 	}
-	// });
-
+	$('.header__banner.owl-carousel').owlCarousel({
+	    loop: true,
+	    nav: false,
+	    dots: false,
+	    autoplay: true,
+	    autoplayTimeout: 7000,
+	    autoplaySpeed: 1200,
+	    items: 1
+	});
 	$('.how-we-work__sidebar li').click(function() {
 		$(this).addClass('active').siblings().removeClass('active');
 		$('.how-we-work__list li').eq($(this).index()).addClass('active').siblings().removeClass('active');
 	})
-	$('.owl-carousel').owlCarousel({
-	    loop:true,
+	$('.team-list').owlCarousel({
+	    loop:false,
 	    nav: true,
 	    dots: true,
 	    center: true,
+	    startPosition: 1,
 	    items: 3,
 	    responsiveClass:true,
 	    responsive:{
 	    }
 	});
+
+	var scrolled;
+	$(window).scroll(function(){
+		scrolled = $(this).scrollTop() + $(window).height();
+		if (scrolled - 300 > $('.why-us').offset().top) {
+			$('.why-us').addClass('in-view');
+		}
+	})
 });
