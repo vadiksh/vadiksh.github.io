fc8122dcadaf49f73a02c4d5372a12ad113b801d
Vadim <vadik.shevchenko@gmail.com>
2019-08-29 01:19:18 +0000

    added services + animations

--- mashora/js/scripts.min.js
+++ mashora/js/scripts.min.js
@@ -1,34 +1,36 @@
 $(function() {
 	var textWrapper = document.querySelector('.mashora');
-	textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+	if ($('.loader').length) {
+		textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-	$('.mashora').css({"opacity": 1})
-	anime.timeline({loop: false})
-	  .add({
-	    targets: '.mashora .letter',
-	    translateY: [50,0],
-	    easing: "easeOutExpo",
-	    duration: 1400,
-	    delay: (el, i) => 100 + 50 * i
-	  }).add({
-	    targets: '.mashora .letter',
-	    duration: 200,
-	    translateY: [0,-50],
-	    easing: "easeInQuad",
-	    delay: (el, i) => 500 + 50 * i
-	  });
-	  
-	  setTimeout(function() {
-	  	 $('.loader').hide();
-	  },3000)
-	  setTimeout(function() {
-	  	$('.header .first-screen').addClass('active');
-	  },1200)
-	  $('.loader .bg').addClass('hidden');
+		$('.mashora').css({"opacity": 1})
+		anime.timeline({loop: false})
+		  .add({
+		    targets: '.mashora .letter',
+		    translateY: [50,0],
+		    easing: "easeOutExpo",
+		    duration: 1400,
+		    delay: (el, i) => 100 + 50 * i
+		  }).add({
+		    targets: '.mashora .letter',
+		    duration: 200,
+		    translateY: [0,-50],
+		    easing: "easeInQuad",
+		    delay: (el, i) => 500 + 50 * i
+		  });
+		  
+		  setTimeout(function() {
+		  	 $('.loader').hide();
+		  },3000)
+		  setTimeout(function() {
+		  	$('.header .first-screen').addClass('active');
+		  },1500)
+		  $('.loader .bg').addClass('hidden');
+
+		$('.header.animated').addClass('visible');
+	}
+	$('.header').addClass('visible');
 	
-
-
-	$('.header.animated').addClass('visible');
 	
 
 	var i = 1;
@@ -46,17 +48,20 @@ $(function() {
 		$('.how-we-work__list li').eq($(this).index()).addClass('active').siblings().removeClass('active');
 	});
 
-	$('.team__list').owlCarousel({
-	    loop:false,
-	    nav: true,
-	    dots: true,
-	    center: true,
-	    startPosition: 1,
-	    items: 3,
-	    responsiveClass:true,
-	    responsive:{
-	    }
-	});
+	if ($('.owl-carousel').length) {
+		$('.team__list').owlCarousel({
+		    loop:false,
+		    nav: true,
+		    dots: true,
+		    center: true,
+		    startPosition: 1,
+		    items: 3,
+		    responsiveClass:true,
+		    responsive:{
+		    }
+		});
+	}
+	
 
 	var scrolled;
 	$(window).scroll(function(){
@@ -64,5 +69,17 @@ $(function() {
 		if ($('.why-us').length && scrolled - 300 > $('.why-us').offset().top) {
 			$('.why-us').addClass('in-view');
 		}
+		for (var i = 0; i < $('.services__list li').length; i++) {
+			if (scrolled - 300 > $('.services__list li').eq(i).offset().top) {
+				$('.services__list li').eq(i).addClass('in-view');
+			}
+		}
+		if ($('.services__title').length && scrolled - 300 > $('.services__title').offset().top) {
+			$('.services__title').addClass('in-view');
+		}
+		if ($('.contact').length && scrolled - 300 > $('.contact').offset().top) {
+			$('.contact').addClass('in-view');
+		}
 	})
+	$(window).trigger('scroll');
 });
