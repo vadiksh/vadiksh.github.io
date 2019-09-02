b2f73ca0ec5943888c1adbd484f08c0caee00361
Vadim Shevchenko <vadik.shevchenko@gmail.com>
2019-09-02 18:13:08 +0300

    delete trash

--- legacy/old-version/wp-content/plugins/image-caption-hover/includes/import_export.php
+++
@@ -1,3 +0,0 @@
-<div class="wrap">
-	Get <a href="http://webcodingplace.com/wordpress-pro-plugins/image-caption-hover-pro-wordpress-plugin/" target="_blank">Pro Version</a>  to Unlock Import/Export Option
-</div>
