dd22d2d18daaacf7c1fe41cb162e2804f63ff1fa
Vadim Shevchenko <vadik.shevchenko@gmail.com>
2019-11-28 23:05:43 +0200

    updated royce

--- royce/js/common.js
+++ royce/js/common.js
@@ -18,6 +18,7 @@ $(function() {
 	var subEntered = false;
 	$('.header__nav-sublink').mouseenter(function(){
 		subEntered = true;
+		$('.header__nav-submenu').addClass('visible');
 		$('.header__nav-submenu > li').eq($(this).index('.header__nav-sublink')).addClass('active').siblings().removeClass('active');
 	})
 	$('.header__nav-sublink').mouseleave(function(){
@@ -25,6 +26,7 @@ $(function() {
 		subEntered = false;
 		setTimeout(function() {
 			if (!subEntered) {
+				$('.header__nav-submenu').removeClass('visible');
 				$('.header__nav-submenu > li').eq(that.index('.header__nav-sublink')).removeClass('active')
 			}
 		},100)
@@ -36,8 +38,42 @@ $(function() {
 		subEntered = false;
 		setTimeout(function() {
 			if (!subEntered) {
+				$('.header__nav-submenu').removeClass('visible');
 				$('.header__nav-submenu > li').removeClass('active')
 			}
 		},200)
+	})
+
+	$('.header .hamburger').click(function() {
+		if (!$(this).hasClass('active')) {
+			$(this).addClass('active');
+			$('.header-mob__search, .header-mob__container').addClass('active');
+		} else {
+			$(this).removeClass('active');
+			$('.header-mob__search, .header-mob__container').removeClass('active');
+			// $('.header-mob, .header-mob__main, .header-mob__subfunds, .header-mob__submenus').removeClass('active hidden');
+			// $('.header-mob__submenus > li, .header-mob__subfunds > li').removeClass('active');
+		}
+		
+	})
+	$('.header-mob__nav li').click(function() {
+		$('.header-mob__submenus > li').eq($(this).index()).addClass('active').siblings().removeClass('active');
+		$('.header-mob__submenus').addClass('active');
+		$('.header-mob__main').addClass('hidden');
+	})
+	$('.header-mob__sublink').click(function(e) {
+		e.preventDefault();
+		$('.header-mob__subfunds > li').eq($(this).index('.header-mob__sublink')).addClass('active').siblings().removeClass('active');
+		$('.header-mob__subfunds').addClass('active');
+		$('.header-mob__submenus').addClass('hidden');
+	})
+	$('.header-mob__back').click(function() {
+		if($(this).parents('.header-mob__submenus').length) {
+			$('.header-mob__submenus').removeClass('active');
+			$('.header-mob__main').removeClass('hidden');
+		} else {
+			$('.header-mob__subfunds').removeClass('active');
+			$('.header-mob__submenus').removeClass('hidden');
+		}
 	})
 });
