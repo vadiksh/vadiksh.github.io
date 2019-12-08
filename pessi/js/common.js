9b0e3ab1d1384c148da085e948f25ea45b6aca51
Vadim Shevchenko <vadik.shevchenko@gmail.com>
2019-12-08 06:00:17 +0200

    ie fixes

--- pessi/js/common.js
+++ pessi/js/common.js
@@ -6,14 +6,14 @@ $(function() {
 		scrolled = $('body, html').scrollTop() + $(window).height();
 		if (scrolled - 200 > $('.achievements').offset().top) {
 			for (var i = 0; i < $('.achievements .number').length; i++) {
-				$($('.achievements .number')[i]).html($('.achievements .number')[i].dataset.number);
+				$($('.achievements .number')[i]).html($($('.achievements .number')[i]).attr('data-number'));
 			}
 		}
 	})
 
 	$('.cards__list > li').mouseenter(function() {
 		for (var i = 0; i < $(this).find('.cards__flip-num').length; i++) {
-			$($(this).find('.cards__flip-num')[i]).html($(this).find('.cards__flip-num')[i].dataset.number);
+			$($(this).find('.cards__flip-num')[i]).html($($(this).find('.cards__flip-num')[i]).attr('data-number'));
 		}
 		
 	})
