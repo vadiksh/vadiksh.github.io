c12a1bea9b1cadfad4c9e2c132940b497a546403
Vadim Shevchenko <vadik.shevchenko@gmail.com>
2019-12-08 05:11:19 +0200

    pessi updated

--- pessi/js/common.js
+++ pessi/js/common.js
@@ -1,5 +1,28 @@
 $(function() {
 
-	// Custom JS
+	
+	var scrolled;
+	$(window).scroll(function() {
+		scrolled = $('body, html').scrollTop() + $(window).height();
+		if (scrolled - 200 > $('.achievements').offset().top) {
+			for (var i = 0; i < $('.achievements .number').length; i++) {
+				$($('.achievements .number')[i]).html($('.achievements .number')[i].dataset.number);
+			}
+		}
+	})
 
+	$('.cards__list > li').mouseenter(function() {
+		for (var i = 0; i < $(this).find('.cards__flip-num').length; i++) {
+			$($(this).find('.cards__flip-num')[i]).html($(this).find('.cards__flip-num')[i].dataset.number);
+		}
+		
+	})
+	$('.cards__list > li').mouseleave(function() {
+		for (var i = 0; i < $(this).find('.cards__flip-num').length; i++) {
+			$($(this).find('.cards__flip-num')[i]).html('0');
+		}
+		
+	})
+	
+	$(window).trigger('scroll');
 });
