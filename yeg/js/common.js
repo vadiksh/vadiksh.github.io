914388dea7557d87a3396de8b873e82184eb1c9c
Vadim Shevchenko <vadik.shevchenko@gmail.com>
2019-12-13 06:05:45 +0200

    yeg added

---
+++ yeg/js/common.js
@@ -0,0 +1,43 @@
+$(function() {
+
+	$('.hamburger').click(function() {
+		$('.header__main-menu').addClass('active');
+	})
+	$('.header .close').click(function() {
+		$('.header__main-menu').removeClass('active');
+	})
+	for (var i = 0; i < $('.slider__list li').length; i++) {
+		var index = i + 1;
+		$($('.slider__list li')[i]).css({
+			"background": "url('img/slide" + index + ".jpg') center center/cover"
+		})
+	}
+	$(window).resize(function() {
+		if ($(window).width() <= 992) {
+			
+			$('.slider__list').owlCarousel({
+				items: 1,
+				loop: true, 
+				autoplay: true,
+				nav: false
+			})
+		} else {
+			$('.slider__list').trigger('destroy.owl.carousel');
+		}
+	})
+	if ($(window).width() <= 992) {
+		for (var i = 0; i < $('.slider__list li').length; i++) {
+			var index = i + 1;
+			$($('.slider__list li')[i]).find('.img').css({
+				"background": "url('img/slide" + index + ".jpg') center top/cover"
+			})
+		}
+		$('.slider__list').owlCarousel({
+			items: 1,
+			loop: true, 
+			autoplay: true,
+			nav: false
+		})
+		
+	}
+});
