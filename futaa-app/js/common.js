7e876f3684543f2db344fca59561eaa42263405d
Vadim Shevchenko <vadik.shevchenko@gmail.com>
2020-01-16 05:29:44 +0200

    added futaa

---
+++ futaa-app/js/common.js
@@ -0,0 +1,24 @@
+$(function() {
+	$('.login__pin input').keyup(function() {
+
+		for (var i = 3; i >= 0; i--) {
+			$($('.login__pin-field')[i]).html($(this).val().charAt(i))
+			if (!$(this).val().charAt(i)) {
+				$($('.login__pin-field')[i]).html('').addClass('active').siblings().removeClass('active');
+			}
+			if($(this).val().length == 4) {
+				$('.login__pin-field').removeClass('active');
+			}
+		}
+	})
+	$('.predict button').click(function(e) {
+		e.preventDefault();
+		var val = $(this).siblings('input').val();
+		if ($(this).hasClass('decrement') && val > 0) {
+			val--;
+		} else if ($(this).hasClass('increment')){
+			val++;
+		}
+		$(this).siblings('input').val(val)
+	})
+});
