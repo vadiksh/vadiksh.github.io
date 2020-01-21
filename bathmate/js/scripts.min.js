e5889b441db5f85a223110435f536b1d369e22a1
Vadim Shevchenko <vadik.shevchenko@gmail.com>
2020-01-21 22:02:07 +0200

    added bathmate

---
+++ bathmate/js/scripts.min.js
@@ -0,0 +1,15 @@
+$(function() {
+
+	$('.banner .owl-carousel').owlCarousel({
+		loop:true,
+		items: 1,
+		nav: false,
+		dots: false,
+		navSpeed: 1000,
+		// autoplay: true,
+		autoplayHoverPause: true,
+		autoplaySpeed: 1000,
+		dragEndSpeed: 1000
+	});
+
+});
