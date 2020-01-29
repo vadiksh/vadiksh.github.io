e56e31dfb1b04b1f5fe625713ef61bb2f23d692c
Vadim Shevchenko <vadik.shevchenko@gmail.com>
2020-01-29 14:58:07 +0200

    bathmate responsive

--- bathmate/js/scripts.min.js
+++ bathmate/js/scripts.min.js
@@ -6,10 +6,36 @@ $(function() {
 		nav: false,
 		dots: false,
 		navSpeed: 1000,
-		// autoplay: true,
+		autoplay: true,
 		autoplayHoverPause: true,
 		autoplaySpeed: 1000,
 		dragEndSpeed: 1000
 	});
-
+	$('.orders input').on("input change", function() {
+		let clicks = $(this).val();
+		let comissions = [3780, 7560, 11340, 15120, 18900, 22680, 26460, 30240, 34020, 37800]
+		console.log(clicks);
+		
+		if (clicks >= 100 && clicks < 200) {
+			$('.orders__comission span').html('$' + comissions[0])
+		} else if (clicks >= 200 && clicks < 300) {
+			$('.orders__comission span').html('$' + comissions[1])
+		} else if (clicks >= 300 && clicks < 400) {
+			$('.orders__comission span').html('$' + comissions[2])
+		} else if (clicks >= 400 && clicks < 500) {
+			$('.orders__comission span').html('$' + comissions[3])
+		} else if (clicks >= 500 && clicks < 600) {
+			$('.orders__comission span').html('$' + comissions[4])
+		} else if (clicks >= 600 && clicks < 700) {
+			$('.orders__comission span').html('$' + comissions[5])
+		} else if (clicks >= 700 && clicks < 800) {
+			$('.orders__comission span').html('$' + comissions[6])
+		} else if (clicks >= 800 && clicks < 900) {
+			$('.orders__comission span').html('$' + comissions[7])
+		} else if (clicks >= 900 && clicks < 1000) {
+			$('.orders__comission span').html('$' + comissions[8])
+		} else {
+			$('.orders__comission span').html('$' + comissions[9])
+		}
+	})
 });
